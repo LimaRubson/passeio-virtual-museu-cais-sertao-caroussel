537ee9f85aaba7cb6c317b50bfa22ba534a7bf11
Rubson Lima <limarubson@hotmail.com>
2018-08-12 20:36:31 -0300

    Criado e implementado as funcionalidade com javascript.

---
+++ js/passeioVirtual.js
@@ -0,0 +1,264 @@
+ //Código que muda as imagens 360º 
+ document.getElementById("image-1").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'true')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'true')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    //document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+
+  //Código que muda as imagens 360º
+   document.getElementById("image-2").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'true')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'true')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    //document.getElementById("indicators-9").setAttribute('visible', 'false')
+    
+  });
+  //Código que muda as imagens 360º
+  document.getElementById("image-3").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'true')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'true')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    //document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+  //Código que muda as imagens 360º 
+  document.getElementById("image-4").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'true')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'true')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+  //Código que muda as imagens 360º
+   document.getElementById("image-5").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'true')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'true')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    //document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+  //Código que muda as imagens 360º
+  document.getElementById("image-6").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'true')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'true')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+   //Código que muda as imagens 360º 
+   document.getElementById("image-7").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'true')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'true')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+  //Código que muda as imagens 360º
+   document.getElementById("image-8").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'true')
+    document.getElementById("image-360-9").setAttribute('visible', 'false')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'true')
+    document.getElementById("indicators-9").setAttribute('visible', 'false')
+
+  });
+  //Código que muda as imagens 360º
+  document.getElementById("image-9").addEventListener("click", function() {
+    document.getElementById("image-360-1").setAttribute('visible', 'false')
+    document.getElementById("image-360-2").setAttribute('visible', 'false')
+    document.getElementById("image-360-3").setAttribute('visible', 'false')
+    document.getElementById("image-360-4").setAttribute('visible', 'false')
+    document.getElementById("image-360-5").setAttribute('visible', 'false')
+    document.getElementById("image-360-6").setAttribute('visible', 'false')
+    document.getElementById("image-360-7").setAttribute('visible', 'false')
+    document.getElementById("image-360-8").setAttribute('visible', 'false')
+    document.getElementById("image-360-9").setAttribute('visible', 'true')
+
+    document.getElementById("indicators-1").setAttribute('visible', 'false')
+    document.getElementById("indicators-2").setAttribute('visible', 'false')
+    document.getElementById("indicators-3").setAttribute('visible', 'false')
+    document.getElementById("indicators-4").setAttribute('visible', 'false')
+    document.getElementById("indicators-5").setAttribute('visible', 'false')
+    document.getElementById("indicators-6").setAttribute('visible', 'false')
+    document.getElementById("indicators-7").setAttribute('visible', 'false')
+    document.getElementById("indicators-8").setAttribute('visible', 'false')
+    document.getElementById("indicators-9").setAttribute('visible', 'true')
+
+  });
+
+  let audioEl = document.querySelector("a-sound");
+  let audio = audioEl.components.sound;
+
+  document.getElementById("indicators-2-botao-1").addEventListener("click", function () {
+    document.getElementById("receiver-botao-2").setAttribute('visible', 'true')
+    play = audio.playSound();
+    if(document.getElementById("indicators-2-botao-1").getAttribute('visible') == true) {
+      document.getElementById("indicators-2-botao-1").setAttribute('visible', 'false')
+    }
+
+  })
+
+  document.getElementById("close-inf-botao-2").addEventListener("click", function() {
+    if(document.getElementById("receiver-botao-2").getAttribute('visible') == true) {
+      document.getElementById("receiver-botao-2").setAttribute('visible', 'false');
+      stop = audio.stopSound();
+      document.getElementById("indicators-2-botao-1").setAttribute('visible', 'true');
+    }
+  });
+
+  /*var i = -90
+
+  document.addEventListener("DOMContentLoaded", function(){
+
+       document.getElementById("image-360").setAttribute('rotation', {x: 0, y: i, z:0})
+
+   
+  })
+
+  function Temporizador(initiate) {
+      if (initiate !== true) {
+          i  = i - 0.007;
+          document.getElementById("image-360").setAttribute('rotation', {x: 0, y: i, z:0})
+          console.log(i);
+      }
+
+      
+      setTimeout(Temporizador, -1000);
+  }
+
+
+    $(function() {
+
+        Temporizador(true); 
+       ) 
+    });*/
+
+//$("#dialog-1").removeClass('intro-overlay-museu');
+
+
+
+
+
+
+       
